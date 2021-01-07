158af4c4b803342a740fe2c58165293b69064659
NathaliaMinoque <jr.minoqueen@gmail.com>
2021-01-07 15:41:52 +0700

    gak diapa2in sih

--- Satuin Sudoku/Satuin Sudoku/Form1.cs
+++ Satuin Sudoku/Satuin Sudoku/Form1.cs
@@ -17,10 +17,12 @@ namespace Satuin_Sudoku
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        public void Form1_Load(object sender, EventArgs e)
         {
             //Ini codingan bikin textbox array
             var soal = new TextBox[10, 10];
+
             for (var i = 1; i < 10; i++)
             {
                 for (var j = 1; j < 10; j++)
@@ -30,11 +32,33 @@ namespace Satuin_Sudoku
                     tbox.Location = new Point(i * 25, j * 25);
                     tbox.Visible = true;
                     tbox.Size = new Size(20, 20);
+                    tbox.ForeColor = Color.Black;
                     this.Controls.Add(tbox);
-                    tbox.Text = " ";
+                    //tbox.Text = "";
                     soal[i, j] = tbox;
                 }
             }
-        }
+            
+            //check kolom
+            int cekkembar = 0;
+            for (int baris = 1; baris < 9; baris++)
+            {
+                for (int kolom = 1; kolom < 9; kolom++)
+                {
+                    for (int counter = 1; counter < 9; counter++)
+                    {
+                        if (soal[baris, kolom] == soal[baris, counter])
+                        {
+                            cekkembar++;
+                        }                      
+                        if (cekkembar > 1)
+                        {
+                            soal[baris, kolom].ForeColor = Color.Red;
+                            soal[baris, counter].ForeColor = Color.Red;
+                        }
+                    }
+                }
+            }
+        }       
     }
 }
